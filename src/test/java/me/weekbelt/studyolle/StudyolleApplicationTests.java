e2195980da5a0eb6b94a6ffe387274226d2dbfeb
joohyuk <vfrvfr4207@gmail.com>
2020-03-30 00:34:58 +0900

    Initial commit

---
+++ src/test/java/me/weekbelt/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
