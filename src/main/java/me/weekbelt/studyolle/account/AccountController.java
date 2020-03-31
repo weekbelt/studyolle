b90afbe7e6ab2b512505940ffee2005426710692
joohyuk <vfrvfr4207@gmail.com>
2020-03-31 23:47:38 +0900

    join form submit

--- src/main/java/me/weekbelt/studyolle/account/AccountController.java
+++ src/main/java/me/weekbelt/studyolle/account/AccountController.java
@@ -1,15 +1,61 @@
 package me.weekbelt.studyolle.account;
 
+import lombok.RequiredArgsConstructor;
+import me.weekbelt.studyolle.domain.Account;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
 @Controller
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model){
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if(errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getEmail())
+                .password(signUpForm.getPassword())     // TODO encoding 해야함
+                .emailVerified(false)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올래, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/me/weekbelt/studyolle/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package me.weekbelt.studyolle.account;
+
+import me.weekbelt.studyolle.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
